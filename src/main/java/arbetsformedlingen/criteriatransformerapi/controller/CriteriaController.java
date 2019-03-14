7fff53f4ff85ff982683c8386d4577a6796af7d1
Serdil Akyuz <serdil.akyuz@arbetsformedlingen.se>
2019-03-14 15:49:55 +0100

    message

--- src/main/java/arbetsformedlingen/criteriatransformerapi/controller/CriteriaController.java
+++ src/main/java/arbetsformedlingen/criteriatransformerapi/controller/CriteriaController.java
@@ -13,19 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@Api(value = "/transform", description = "Transform elise criteria to elastic criteria.")
+@Api(value = "/transform", description = "Transform elise criteria to sok-api criteria.")
 public class CriteriaController {
 
     @Autowired
     private ICriteriaService service;
 
-    @ApiOperation(value = "Transform elise criteria message to a Criteria for sok-api")
+    @ApiOperation(value = "transform elise criteria to sok-api criteria")
     @RequestMapping(value = "/transform", method = RequestMethod.POST, produces = "application/json")
     public Mono<Criteria> translateToSokApiCriteria(@RequestBody MatchningsparametrarDTO dto) {
         return service.transformToCriteria(dto);
     }
 
-    @ApiOperation(value = "Transform elise criteria message to a queryPath for sok-api")
+    @ApiOperation(value = "transform elise criteria to sok-api queryPath")
     @RequestMapping(value = "/transform/querypath", method = RequestMethod.POST)
     public Mono<String> translateToQueryPath(@RequestBody MatchningsparametrarDTO dto) {
         return service.transformToQueryPath(dto);
