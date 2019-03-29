7cd80611feb053fd5a865be74ae0d6e6a794f07e
Serdil Akyuz <serdil.akyuz@arbetsformedlingen.se>
2019-03-29 14:05:38 +0100

    added more profile, testing with code

--- src/main/java/arbetsformedlingen/criteriatransformerapi/controller/CriteriaController.java
+++ src/main/java/arbetsformedlingen/criteriatransformerapi/controller/CriteriaController.java
@@ -5,7 +5,11 @@ import arbetsformedlingen.criteriatransformerapi.elisecriteria.Matchningsparamet
 import arbetsformedlingen.criteriatransformerapi.service.ICriteriaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,11 @@ import reactor.core.publisher.Mono;
 @RestController
 @Api(value = "/transform")
 public class CriteriaController {
+
+    private Logger logger = LoggerFactory.getLogger(CriteriaController.class);
+
+    @Value("${miljo}")
+    private String miljo;
 
     @Autowired
     private ICriteriaService service;
@@ -28,6 +37,16 @@ public class CriteriaController {
     @PostMapping(value = "/transform/querypath")
     public Mono<String> translateToQueryPath(@RequestBody MatchningsparametrarDTO dto) {
         return service.transformToQueryPath(dto);
+    }
+
+    @GetMapping("/miljo")
+    public String index() {
+        logger.trace("picked up variable: " + miljo);
+        logger.debug("picked up variable: " + miljo);
+        logger.info("picked up variable: " + miljo);
+        logger.warn("picked up variable: " + miljo);
+        logger.error("picked up variable: " + miljo);
+        return "check logs";
     }
 
 }
