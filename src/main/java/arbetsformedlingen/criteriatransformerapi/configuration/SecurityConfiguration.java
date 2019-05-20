d373a026ad83d08849492fa8fb0dca7442d45bf3
Serdil Akyuz <serdil.akyuz@arbetsformedlingen.se>
2019-05-20 22:27:26 +0200

    enables security

--- src/main/java/arbetsformedlingen/criteriatransformerapi/configuration/SecurityConfiguration.java
+++ src/main/java/arbetsformedlingen/criteriatransformerapi/configuration/SecurityConfiguration.java
@@ -1,11 +1,46 @@
 package arbetsformedlingen.criteriatransformerapi.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
-@Configuration
 public class SecurityConfiguration {
+
+    private static final String API_KEY = "api-key";
+    private static final String USER = "USER";
+
+    @Value("${API_TOKEN}")
+    private String token;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername(API_KEY)
+                .password(passwordEncoder().encode(token))
+                .roles(USER)
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
