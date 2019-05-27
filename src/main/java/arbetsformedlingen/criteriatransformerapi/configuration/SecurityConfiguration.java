69a57fb5b0e2d2dc675f6896aebc2d044ae73bf5
Serdil Akyuz <serdil.akyuz@arbetsformedlingen.se>
2019-05-27 15:10:22 +0200

    reverting

--- src/main/java/arbetsformedlingen/criteriatransformerapi/configuration/SecurityConfiguration.java
+++ src/main/java/arbetsformedlingen/criteriatransformerapi/configuration/SecurityConfiguration.java
@@ -1,21 +1,59 @@
 package arbetsformedlingen.criteriatransformerapi.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
+    private static final String API_KEY = "api-key";
+    private static final String USER = "USER";
+
+    @Value("${API_TOKEN:password}")
+    private String token;
+
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChainTest(ServerHttpSecurity httpSecurity) {
-        return httpSecurity.authorizeExchange().anyExchange().permitAll()
+    @Profile("prod")
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .anyExchange().authenticated()
                 .and()
-                .httpBasic().and()
-                .cors()
+                .httpBasic()
                 .and()
                 .csrf().disable()
                 .build();
+    }
+
+    @Bean
+    @Profile("!prod")
+    public SecurityWebFilterChain springSecurityFilterChainTest(ServerHttpSecurity httpSecurity) {
+        return httpSecurity.authorizeExchange().anyExchange().permitAll()
+                .and()
+                .httpBasic().and().csrf().disable()
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername(API_KEY)
+                .password(passwordEncoder().encode(token))
+                .roles(USER)
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
